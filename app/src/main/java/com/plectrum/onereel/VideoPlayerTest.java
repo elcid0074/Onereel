be320b73475401d874c75d867b4bde359527eeb5
Williams Abiola <wabiola007@gmail.com>
2017-10-12 16:25:21 +0100

    Project Commit (0.1% Complete)

---
+++ app/src/main/java/com/plectrum/onereel/VideoPlayerTest.java
@@ -0,0 +1,13 @@
+package com.plectrum.onereel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPlayerTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player_test);
+    }
+}
